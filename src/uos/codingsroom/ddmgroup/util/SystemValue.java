27be415a0958beef61f48f60bf6ed03f7e4477bd
JYPark88 <hiphopgn@naver.com>
2014-04-28 05:03:07 +0900

    시스템 밸류 수정

--- src/uos/codingsroom/ddmgroup/util/SystemValue.java
+++ src/uos/codingsroom/ddmgroup/util/SystemValue.java
@@ -7,6 +7,8 @@ public class SystemValue {
 	public static final boolean isAdmin = true;
 
 	public static final String[] kinds = { "장년", "봉사", "청년" };
+	
+	public static final Boolean test = false;
 
 	// public static final String conn = "http://14.63.199.3/security_map/security_map.php?menu=";
 	public static final String conn = "http://14.63.199.182/ddmgroup/ddmgroup.php?menu=";
