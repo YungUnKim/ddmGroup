2b0bb5bb805fe1e86caf0a969f12c8e57b89ea00
JYPark88 <hiphopgn@naver.com>
2014-05-13 16:04:14 +0900

    글 수정 액티비티 뷰 구성

---
+++ src/uos/codingsroom/ddmgroup/ModifyActivity.java
@@ -0,0 +1,93 @@
+package uos.codingsroom.ddmgroup;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.kakao.APIErrorResult;
+import com.kakao.AppActionBuilder;
+import com.kakao.GlobalApplication;
+import com.kakao.KakaoLink;
+import com.kakao.KakaoParameterException;
+import com.kakao.KakaoTalkLinkMessageBuilder;
+import com.kakao.LogoutResponseCallback;
+import com.kakao.UnlinkResponseCallback;
+import com.kakao.UserManagement;
+
+public class ModifyActivity extends Activity implements OnClickListener {
+
+	ImageView backButton;
+
+	TextView groupNameText;
+	EditText titleText;
+	EditText articleText;
+
+	Button imageRemoveButton;
+	Button imageModifyButton;
+	Button backButton2;
+	Button modifyButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		initializeView();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_modify_back:
+			finish();
+			break;
+		case R.id.button_content_back_modify:
+			finish();
+			break;
+		case R.id.button_img_remove:
+
+			break;
+		case R.id.button_img_modify:
+
+			break;
+		case R.id.button_content_modify:
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void initializeView() {
+		setContentView(R.layout.activity_modify);
+
+		backButton = (ImageView) findViewById(R.id.button_modify_back);
+		backButton.setOnClickListener(this);
+		backButton2 = (Button) findViewById(R.id.button_content_back_modify);
+		backButton2.setOnClickListener(this);
+
+		groupNameText = (TextView) findViewById(R.id.text_register_groupname_modify);
+		titleText = (EditText) findViewById(R.id.editTitle_modify);
+		articleText = (EditText) findViewById(R.id.editMemo_modify);
+
+		imageRemoveButton = (Button) findViewById(R.id.button_img_remove);
+		imageRemoveButton.setOnClickListener(this);
+		imageModifyButton = (Button) findViewById(R.id.button_img_modify);
+		imageModifyButton.setOnClickListener(this);
+		modifyButton = (Button) findViewById(R.id.button_content_modify);
+		modifyButton.setOnClickListener(this);
+	}
+
+}
