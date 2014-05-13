9f7a593ede11d47dd3909b574ddce8e6900046b2
JYPark88 <hiphopgn@naver.com>
2014-05-13 15:26:08 +0900

    UI 구성 및 웹 이미지 불러오기 class 생성

--- src/uos/codingsroom/ddmgroup/NoticeActivity.java
+++ src/uos/codingsroom/ddmgroup/NoticeActivity.java
@@ -10,28 +10,29 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 public class NoticeActivity extends Activity implements OnClickListener {
-	
+
 	private ListView noticeListView;
 	private NoticeListAdapter noticeListAdapter;
-	
+
 	private ImageView backButton;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notice);
-		
+
 		noticeListView = (ListView) findViewById(R.id.listview_notice);
 		noticeListAdapter = new NoticeListAdapter(this);
-				
+
 		backButton = (ImageView) findViewById(R.id.button_notice_back);
 		backButton.setOnClickListener(this);
-		
+
+//		new UrlImageDownloadTask(testImage).execute("http://joongangdaily.joins.com/_data/photo/2010/01/25080514.jpg");
+
 		setListView();
-	}
-	
-	public void setListView(){
+	}	
+
+	public void setListView() {
 		noticeListAdapter.addItem(new NoticeItem(0, 1, "아아 공지사항 말씀드립니다. 1", "880803"));
 		noticeListAdapter.addItem(new NoticeItem(0, 3, "아아 공지사항 말씀드립니다. 2", "880803"));
 		noticeListAdapter.addItem(new NoticeItem(0, 12, "아아 공지사항 말씀드립니다. 3", "880803"));
@@ -43,8 +44,7 @@ public class NoticeActivity extends Activity implements OnClickListener {
 		noticeListAdapter.addItem(new NoticeItem(0, 9, "아아 공지사항 말씀드립니다. 5", "880803"));
 		noticeListAdapter.addItem(new NoticeItem(0, 7, "아아 공지사항 말씀드립니다. 6", "880803"));
 		noticeListAdapter.addItem(new NoticeItem(0, 6, "아아 공지사항 말씀드립니다. 7", "880803"));
-		
-		
+
 		noticeListView.setAdapter(noticeListAdapter);
 	}
 
